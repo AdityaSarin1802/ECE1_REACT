3c1895456f9732a17e6375645113f67a82641d5c
AdityaSarin1802 <sarin.aditya18@gmail.com>
2026-05-12 17:48:34 +0530

    Experiment-4

--- Experiment-4/src/App.jsx
+++ Experiment-4/src/App.jsx
@@ -3,16 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import Student from './components/Student'
-
+import Student from './component/Student'
 function App() {
+
 
   return (
     <>
-    <h1 classname="h1">Student Information</h1>
-    <Student  classname="student" name="John Doe" marks={60} major="computer science"/>
-    <Student classname="student" name="Jane Smith" marks={72} major="mathematics"/>
-    <Student classname="student" name="Bob Johnson" marks={61} major="physics"/>
+      <h1 class="h1"  >Student Information card</h1>
+    <Student  classname="student1" name="John Doe" marks={60} major="computer science"/>
+    <Student classname="student1" name="Jane Smith" marks={72} major="mathematics"/>
+    <Student classname="student1" name="Bob Johnson" marks={61} major="physics"/>
     </>
   )
 }
